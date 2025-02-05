b2bd0a68cc37629c23b3280bd5062ec342500def
BraydenWhitmire05 <bwhitmire958@gmail.com>
2025-02-05 17:53:51 -0500

    Update PizzaShop.java

--- PizzaShop.java
+++ PizzaShop.java
@@ -1,7 +1,7 @@
 package intec_2150;
 public class PizzaShop {
 	public static void main(String[] args) {
-	
+	// if there is anything i missed could you give me feedback so I can fix it
 	Pizza p1 = new Pizza ("Normal Pizza",1,1,1.0);
 	System.out.println(p1);
 	
